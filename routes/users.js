043beba751b956340e11e85bc9708bbb9ef80f70
Leo <leopapilaya@gmail.com>
2023-03-02 10:15:06 +0800

    Pretty

--- routes/users.js
+++ routes/users.js
@@ -7,81 +7,74 @@ const db = require("../db");
 
 router.get("/", (req, res) => {
     // console.log(req.query.name);
-  // res.json({
-  //   status: 200,
-  //   message: "Users List",
-  //   data: users
-  // });
-
-  
-
-const sql = "SELECT * FROM clients";
-
-db.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log(result);
     // res.json({
-    //     status: 200,
-    //     message: "Users List",
-    //     data: result[0].name
-    //   });
+    //   status: 200,
+    //   message: "Users List",
+    //   data: users
+    // });
 
-    res.render("users/list", {data: result});
+    const sql = "SELECT * FROM clients";
 
-});
+    db.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        // res.json({
+        //     status: 200,
+        //     message: "Users List",
+        //     data: result[0].name
+        //   });
 
+        res.render("users/list", {data: result});
+    });
 });
 
 router.get("/new", (req, res) => {
-  res.render("users/new")
+    res.render("users/new");
 });
 
 router.post("/", (req, res) => {
-    const isValid = true
-    if(isValid){
-        users.push({firstName: req.body.firstName})
-        res.redirect(`/users/${users.length}`)
+    const isValid = true;
+    if (isValid) {
+        users.push({firstName: req.body.firstName});
+        res.redirect(`/users/${users.length}`);
     } else {
-        res.render("users/new", {firstName: req.body.firstName})
+        res.render("users/new", {firstName: req.body.firstName});
     }
 
-//   res.json({
-//     status: 200,
-//     message: "Create User",
-//     first_name: req.body.firstName
-//   });
+    //   res.json({
+    //     status: 200,
+    //     message: "Create User",
+    //     first_name: req.body.firstName
+    //   });
 });
 
 router
-  .route("/:userId")
-  .get((req, res) => {
-    
-    res.json({
-      status: 200,
-      message: "Detail",
-      user_id: users[req.params.userId-1],
+    .route("/:userId")
+    .get((req, res) => {
+        res.json({
+            status: 200,
+            message: "Detail",
+            user_id: users[req.params.userId - 1],
+        });
+    })
+    .put((req, res) => {
+        res.json({
+            status: 200,
+            message: "Update User",
+            user_id: req.params.userId,
+        });
+    })
+    .delete((req, res) => {
+        res.json({
+            status: 200,
+            message: "Delete User",
+            user_id: req.params.userId,
+        });
     });
-  })
-  .put((req, res) => {
-    res.json({
-      status: 200,
-      message: "Update User",
-      user_id: req.params.userId,
-    });
-  })
-  .delete((req, res) => {
-    res.json({
-      status: 200,
-      message: "Delete User",
-      user_id: req.params.userId,
-    });
-  });
-
-
 
 router.param("id", (req, res, next, id) => {
-    req.user = users[id]
-    next()
-})
+    req.user = users[id];
+    next();
+});
 
 module.exports = router;
